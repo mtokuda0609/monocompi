f3a09a2f957daf99a334e9e802d141ff9c216f27
Masato Tokuda <mtokuda0609@gmail.com>
2017-06-29 10:18:07 +0000

    kansei 02

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.where(user_id: current_user)
+    @items = Item.where(user_id: current_user).order(created_at: :desc).page(params[:page]).per(10)
   end
 
   def show
@@ -38,15 +38,23 @@ class ItemsController < ApplicationController
     # 1. どの商品が選択されているかを判断する
     #Rails.logger.info(name)
     #binding.pry
+    name = params[:item][:name]
+    rk_code = params[:item][:rk_code]
+    ys_code = params[:item][:ys_code]
     
-    @item = Item.new(registration)
-
-    if @item.save
-      flash[:success] = "アイテムを追加しました。"
-      redirect_to items_path
-    else
-      flash.now[:danger] = "アイテムを追加できませんでした。"
+    if name == "" || rk_code == "" || ys_code == ""
+      @item = Item.new
+      flash.now[:danger] = "アイテムを選択し、アイテム名を入力してください。"
       render :new
+    else
+      @item = Item.new(registration)
+      if @item.save
+        flash[:success] = "アイテムを追加しました。"
+        redirect_to items_path
+      else
+        flash.now[:danger] = "アイテムを追加できませんでした。"
+        render :new
+      end
     end
   end
 
@@ -84,7 +92,7 @@ class ItemsController < ApplicationController
     rk_results = RakutenWebService::Ichiba::Item.search(itemCode: rk_code)
     rk_result = rk_results.first
     ys_result = Yahoo::Api.get(Yahoo::Api::Shopping::ItemLookup,{ itemcode: ys_code })
-
+    
     rk_code = rk_result.code
     rk_name = rk_result['itemName']
     rk_price = rk_result['itemPrice']
@@ -96,7 +104,7 @@ class ItemsController < ApplicationController
     ys_url = ys_result['ResultSet']['0']['Result']['0']['Url']
     ys_image_url = ys_result['ResultSet']['0']['Result']['0']['Image']['Small']
     benefit = (rk_price - ys_price).abs
-    
+
     return {
       name: name,
       user_id: user_id,
@@ -116,7 +124,7 @@ class ItemsController < ApplicationController
 
 
   def item_params
-    params.require(:item).permit(:name, :user_id, :benefit, :stock , :rk_code, :rk_name, :rk_price, :rk_url, :rk_image_url)
+    params.require(:item).permit(:name, :user_id, :rk_code, :ys_code)
   end
   
 end
